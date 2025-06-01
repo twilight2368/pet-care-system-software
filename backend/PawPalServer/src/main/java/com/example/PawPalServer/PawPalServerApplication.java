c4748498e49afee9f0aec55475978bb3d0885621
Anonymous <103519498+twilight2368@users.noreply.github.com>
2025-06-01 18:56:20 +0700

    now

---
+++ backend/PawPalServer/src/main/java/com/example/PawPalServer/PawPalServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PawPalServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawPalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawPalServerApplication.class, args);
+	}
+
+}
---
+++ backend/PawPalServer/src/test/java/com/example/PawPalServer/PawPalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PawPalServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawPalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
